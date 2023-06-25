9b84810a16e75d7f09a2ffc41253c09010d0fcd8
Margen Peterson <margen@tlu.ee>
2023-06-25 14:41:03 +0300

    ts errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,6 +30,9 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
   },
 };
 
